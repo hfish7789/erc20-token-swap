177889b8774ff848be1052b606b409f078b8748c
hfish7789 <hfish7789@gmail.com>
2022-06-19 00:34:49 +0900

    pair interface contract update

--- contracts/interfaces/IUniswapV2Pair.sol
+++ contracts/interfaces/IUniswapV2Pair.sol
@@ -6,19 +6,7 @@ interface IUniswapV2Pair {
 
     function token1() external view returns (address);
 
-    function getReserves()
-        external
-        view
-        returns (
-            uint112 reserve0,
-            uint112 reserve1,
-            uint32 blockTimestampLast
-        );
+    function getReserves() external view returns (uint112 reserve0, uint112 reserve1, uint32 blockTimestampLast);
 
-    function swap(
-        uint amount0Out,
-        uint amount1Out,
-        address to,
-        bytes calldata data
-    ) external;
+    function swap(uint amount0Out, uint amount1Out, address to, bytes calldata data) external;
 }
